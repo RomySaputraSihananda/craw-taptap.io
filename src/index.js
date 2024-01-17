0b6d97c4f263e3645413cef6d9651c3683e2b9fd
romys <romysaputrasihanandaa@gmail.com>
2024-01-17 23:03:54 +0700

    update

--- src/index.js
+++ src/index.js
@@ -9,12 +9,11 @@ class Taptap {
 
   constructor() {
     this.#start();
-    // this.#process("33655968");
+    // this.#process("178299");
   }
 
   async #start() {
     for (const platform of this.#platforms) {
-      // this.#platforms.forEach(async (platform) => {
       let i = 0;
       while (true) {
         const requests = await fetch(
@@ -24,8 +23,7 @@ class Taptap {
               limit: 20,
               platform,
               type_name: "hot",
-              "X-UA":
-                "V=1&PN=WebAppIntl2&LANG=en_US&VN_CODE=114&VN=0.1.0&LOC=CN&PLT=PC&DS=Android&UID=4df88e5d-b4f4-4173-8985-a83672c5d35a&CURR=ID&DT=PC&OS=Linux&OSV=x86_64",
+              "X-UA": this.#xua,
             })
         );
 
@@ -33,7 +31,6 @@ class Taptap {
 
         for (const { app } of data.list) {
           await this.#process(app.id);
-          // console.log(app.id);
         }
 
         if (!data.next_page.length) break;
@@ -53,6 +50,15 @@ class Taptap {
 
     const content = await response.json();
     const { app } = content.data;
+    const link = `${this.#BASE_URL}/app/${app.id}`;
+
+    const log = {
+      source: link,
+      total_data: 0,
+      total_data_berhasil_diproses: 0,
+      total_data_gagal_diproses: 0,
+      PIC: "romy",
+    };
 
     console.log("try", app.title);
 
@@ -61,114 +67,122 @@ class Taptap {
       const reviews = await this.#requestReview({
         app_id: app.id,
         from: i,
-        limit: 20,
+        limit: 50,
       });
 
       if (!reviews) break;
+      log.total_data += reviews.length;
 
-      reviews.forEach(async ({ post: postIn }) => {
-        const response = await fetch(
-          `${this.#BASE_URL}/webapiv2/creation/post/v1/detail?` +
-            new URLSearchParams({
-              id_str: postIn.id_str,
-              "X-UA": this.#xua,
-            })
-        );
+      await Promise.all(
+        reviews.forEach(async ({ post: postIn }) => {
+          const response = await fetch(
+            `${this.#BASE_URL}/webapiv2/creation/post/v1/detail?` +
+              new URLSearchParams({
+                id_str: postIn.id_str,
+                "X-UA": this.#xua,
+              })
+          );
 
-        const { data } = await response.json();
-        const { post } = data;
+          const { data } = await response.json();
+          const { post } = data;
+          const { user, id } = post;
 
-        const username = post.user.name
-          .replaceAll("/", "")
-          .replaceAll("\n", "");
-        const stat = !Object.keys(post.stat).length ? null : post.stat;
-        let rating;
-        try {
-          rating = postIn.list_fields.app_ratings
-            ? postIn.list_fields.app_ratings[app.id].score
-            : null;
-        } catch (e) {
-          rating =
-            postIn.list_fields.app_ratings[
-              Object.keys(postIn.list_fields.app_ratings)[0]
-            ].score;
-        }
+          const username = user.name.replaceAll("/", "").replaceAll("\n", "");
+          const stat = !Object.keys(post.stat).length ? null : post.stat;
 
-        const outputFile = `data/${app.title}/${username}.json`;
-        try {
-          this.writeFile(outputFile, {
-            link: `${this.#BASE_URL}/app/${app.id}`,
-            domain: this.#BASE_URL.split("/")[2],
-            tag: `${this.#BASE_URL}/app/${app.id}`.split("/").slice(2),
-            crawling_time: strftime("%Y-%m-%d %H:%M:%S", new Date()),
-            crawling_time_epoch: Date.now(),
-            path_data_raw: `data/data_raw/data_review/www.taptap.io/${app.title}/json/${username}.json`,
-            path_data_clean: `data/data_clean/data_review/www.taptap.io/${app.title}/json/${username}.json`,
-            reviews_name: app.title,
-            description_reviews: app.description.text,
-            developers_reviews: app.developers.map(
-              (developer) => developer.name
-            ),
-            tags_reviews: app.tags ? app.tags.map((tag) => tag.value) : null,
-            location_reviews: null,
-            category_reviews: "application",
-            total_reviews: app.stat.review_count,
-            total_fans: app.stat.fans_count,
-            total_user_want: app.stat.user_want_count,
-            total_user_played: app.stat.user_played_count,
-            total_user_playing: app.stat.user_playing_count,
-            reviews_rating: {
-              total_rating: parseFloat(app.stat.rating.score),
-              detail_total_rating: null,
-            },
-            review_info: app.stat.vote_info,
-            detail_reviews: {
-              username_reviews: username,
-              gender_reviews: post.user.gender.length ? post.user.gender : null,
-              avatar_reviews: post.user.avatar,
-              image_reviews: Object.keys(post.files.images),
-              created_time: strftime(
-                "%Y-%m-%d %H:%M:%S",
-                new Date(post.published_time * 1000)
+          let rating;
+          try {
+            rating = postIn.list_fields.app_ratings
+              ? postIn.list_fields.app_ratings[app.id].score
+              : null;
+          } catch (e) {
+            rating =
+              postIn.list_fields.app_ratings[
+                Object.keys(postIn.list_fields.app_ratings)[0]
+              ].score;
+          }
+
+          const outputFile = `data/${app.title}/${username}_${id}.json`;
+          try {
+            this.writeFile(outputFile, {
+              link,
+              domain: this.#BASE_URL.split("/")[2],
+              tag: `${this.#BASE_URL}/app/${app.id}`.split("/").slice(2),
+              crawling_time: strftime("%Y-%m-%d %H:%M:%S", new Date()),
+              crawling_time_epoch: Date.now(),
+              path_data_raw: `data/data_raw/data_review/www.taptap.io/${app.title}/json/${username}_${id}.json`,
+              path_data_clean: `data/data_clean/data_review/www.taptap.io/${app.title}/json/${username}_${id}.json`,
+              reviews_name: app.title,
+              description_reviews: app.description.text,
+              developers_reviews: app.developers.map(
+                (developer) => developer.name
               ),
-              created_time_epoch: post.published_time,
-              edited_time: strftime(
-                "%Y-%m-%d %H:%M:%S",
-                new Date(post.edited_time * 1000)
-              ),
-              edited_time_epoch: post.edited_time,
-              email_reviews: null,
-              company_name: null,
+              tags_reviews: app.tags ? app.tags.map((tag) => tag.value) : null,
               location_reviews: null,
-              title_detail_reviews: post.title,
-              reviews_rating: rating,
-              detail_reviews_rating: null,
-              total_likes_reviews: stat ? stat.ups | 0 : 0,
-              total_dislikes_reviews: null,
-              total_reply_reviews: stat ? stat.comments | 0 : 0,
-              total_favorites_reviews: stat ? stat.favorites | 0 : 0,
-              content_reviews: post.contents.json
-                .filter((content) => content.type == "paragraph")
-                .map((content) => content.children.map((e) => e.text).join(""))
-                .filter((e) => e != "")
-                .join("\n"),
-              reply_content_reviews: !(stat && stat.comments)
-                ? []
-                : await this.#getReplys({
-                    post_id_str: postIn.id_str,
-                  }),
+              category_reviews: "application",
+              total_reviews: app.stat.review_count,
+              total_fans: app.stat.fans_count,
+              total_user_want: app.stat.user_want_count,
+              total_user_played: app.stat.user_played_count,
+              total_user_playing: app.stat.user_playing_count,
+              reviews_rating: {
+                total_rating: parseFloat(app.stat.rating.score),
+                detail_total_rating: null,
+              },
+              review_info: app.stat.vote_info,
+              detail_reviews: {
+                username_reviews: username,
+                gender_reviews: user.gender.length ? user.gender : null,
+                avatar_reviews: user.avatar,
+                image_reviews: Object.keys(post.files.images),
+                created_time: strftime(
+                  "%Y-%m-%d %H:%M:%S",
+                  new Date(post.published_time * 1000)
+                ),
+                created_time_epoch: post.published_time,
+                edited_time: strftime(
+                  "%Y-%m-%d %H:%M:%S",
+                  new Date(post.edited_time * 1000)
+                ),
+                edited_time_epoch: post.edited_time,
+                email_reviews: null,
+                company_name: null,
+                location_reviews: null,
+                title_detail_reviews: post.title,
+                reviews_rating: rating,
+                detail_reviews_rating: null,
+                total_likes_reviews: stat ? stat.ups | 0 : 0,
+                total_dislikes_reviews: null,
+                total_reply_reviews: stat ? stat.comments | 0 : 0,
+                total_favorites_reviews: stat ? stat.favorites | 0 : 0,
+                content_reviews: post.contents.json
+                  .filter((content) => content.type == "paragraph")
+                  .map((content) =>
+                    content.children.map((e) => e.text).join("")
+                  )
+                  .filter((e) => e != "")
+                  .join("\n"),
+                reply_content_reviews: !(stat && stat.comments)
+                  ? []
+                  : await this.#getReplys({
+                      post_id_str: postIn.id_str,
+                    }),
 
-              date_of_experience: null,
-              date_of_experience_epoch: null,
-            },
-          });
-        } catch (e) {
-          console.log(e);
-        }
-      });
-      i += 20;
+                date_of_experience: null,
+                date_of_experience_epoch: null,
+              },
+            });
+            log.total_data_berhasil_diproses += 1;
+            console.log(outputFile);
+          } catch (e) {
+            log.total_data_gagal_diproses += 1;
+          }
+        })
+      );
+      i += 50;
     }
     console.log("success", app.title);
+    fs.appendFile("log.txt", JSON.stringify(log) + "\n");
   }
 
   async writeFile(outputFile, data) {
